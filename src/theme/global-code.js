e6815e8b795e5d91afade9fef952c00336d13b3f
mstenersf <103209646+mstenersf@users.noreply.github.com>
2022-05-19 09:40:33 -0700

    Adding CSS and Consolidating JS

--- src/theme/global-code.js
+++ src/theme/global-code.js
@@ -118,6 +118,18 @@
       });
     /* END Samples Specific Code */
 
+    /* START Google Tag Manager */
+    var dataLayer = [];
+    (
+        function(w,d,s,l,i){
+          w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        }
+    )
+    (window,document,'script','dataLayer','GTM-BVCN');
+    /* END Google Tag Manager */
 
     /* START Catalog Bottom Section */
       $('#catalog-content').append($('#bottom-section').detach());
