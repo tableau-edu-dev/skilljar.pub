9422178dfd89703ba08dd07ad76b718cd7a53103
mstenersf <103209646+mstenersf@users.noreply.github.com>
2022-06-10 11:06:10 -0700

    Test CSS injection from JS

--- src/pages/catalog/catalog.js
+++ src/pages/catalog/catalog.js
@@ -7,6 +7,22 @@ function scrollThere(targetElement) {
     }, 1000, 'swing');
 }
 
+// Get HTML head element
+var head = document.getElementsByTagName('HEAD')[0];
+
+// Create new link Element
+var link = document.createElement('link');
+
+// set the attributes for link element
+link.rel = 'stylesheet';
+
+link.type = 'text/css';
+
+link.href = 'https://tableau-edu-dev.github.io/skilljar.pub/src/pages/catalog/catalog.css';
+
+// Append link element to HTML head
+head.appendChild(link);
+
 $(document).ready(function() {
         if (document.documentElement.lang == 'en-US') {
             //Course header
