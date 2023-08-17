d31f33c209238c4f6628f304b1c5358c42099bd1
mstenersf <103209646+mstenersf@users.noreply.github.com>
2023-08-17 15:14:45 -0700

    Additional Optional Tiles

---
+++ src/paths/server_architect.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  var SA = generatedTiles["ServerArch"];
+  var newTile = generateCourseTile(SA["href"], SA["title"], SA["image"], SA["desc"], SA["tags"], "_blank");
+  $(newTile).insertAfter("a[data-oid='2ga8e77oxfnji']");
+});
